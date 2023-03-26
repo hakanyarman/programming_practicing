bb2ffaf407ebad282070872ae96ceaa83dfc3758
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-03-26 23:08:06 +0300

    call,apply,bind

--- static/jsNote.js
+++ static/jsNote.js
@@ -782,3 +782,48 @@ bir event içinde this , eventi alan elemanı temsil eder
 
 this tek başına kullanıldığında global i temsil eder.
 */
+
+//-------------------Call,Apply,Bind-----------------------------------
+
+var welcome = function(){
+  console.log("Welcome " + this.name );
+}
+
+console.log(welcome());
+
+var yigit = {
+  name : "Yiğit"
+}
+
+var ada = {
+  name : "Ada"
+}
+
+console.log(welcome.call(yigit)); //Welcome Yiğit
+console.log(welcome.call(ada));
+
+console.log(welcome.apply(yigit)); //Welcome Yiğit
+// fonksiyon eğer parametre almıyorsa call ve apply aynıdır.
+
+console.log(welcome.bind(yigit)); //[Function: bound welcome]
+// bu sefer yigit objecti için yeni fonksiyon oluşturdu. 
+// parametre olarak yazdığımız object için yeni fonksiyon oluşturdu 
+// ve fonksiyonu döndürdü
+
+ var welcomeYigit = welcome.bind(yigit);
+
+ console.log(welcomeYigit()); //Welcome Yiğit 
+
+ //----------Call & Apply farkı -----------------
+
+ var greeting = function(a,b){
+    console.log("Hello "+ this.name + " are you interested in " + a + " and " + b);
+ }
+
+ var greetingToYigit = greeting.call(yigit,"football","basketball");
+
+ // call ile apply in tek farkı : fonksiyonda parametre olması durumunda 
+ // apply ile parametreleri array olarak yazmamız gerek.
+ // call da direkt yazarız.
+ var greetingToAda = greeting.apply(ada,["football","basketball"]);
+
--- static/tempCodeRunnerFile.js
+++ static/tempCodeRunnerFile.js
@@ -1,2 +1,37 @@
-console.log(e);
-let e = 5;
+var welcome = function(){
+  console.log("Welcome " + this.name );
+}
+
+console.log(welcome());
+
+var yigit = {
+  name : "Yiğit"
+}
+
+var ada = {
+  name : "Ada"
+}
+
+console.log(welcome.call(yigit)); //Welcome Yiğit
+console.log(welcome.call(ada));
+
+console.log(welcome.apply(yigit)); //Welcome Yiğit
+// fonksiyon eğer parametre almıyorsa call ve apply aynıdır.
+
+console.log(welcome.bind(yigit)); //[Function: bound welcome]
+// bu sefer yigit objecti için yeni fonksiyon oluşturdu. 
+// parametre olarak yazdığımız object için yeni fonksiyon oluşturdu 
+// ve fonksiyonu döndürdü
+
+ var welcomeYigit = welcome.bind(yigit);
+
+ console.log(welcomeYigit()); //Welcome Yiğit 
+
+ //----------Call & Apply farkı -----------------
+
+ var greeting = function(a,b){
+    console.log("Hello "+ this.name + " are you interested in " + a + " and " + b);
+ }
+
+ var greetingToYigit = greeting.call(yigit,"football","basketball");
+ var greetingToAda = greeting.apply(ada,["football","basketball"]);
