be86559c3f98b639a46eebd187c9e4d0bc1bb2f9
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-04-03 01:57:29 +0300

    object prototypes

--- static/tempCodeRunnerFile.js
+++ static/tempCodeRunnerFile.js
@@ -1,53 +1,36 @@
-
-const human = {name:"Hakan",surname:"Yarman",age:21};
-const man = human;
-man.name = "Ahmet";
-// burada human objectinin kopyası olan man i değiştirdiğimizde human da değişir.
-// çünkü man = human dediğimizde yeni object oluşturulmadı, var olan object e bir tane daha referans eklendi.
-console.log(human); // { name: 'Ahmet', surname: 'Yarman', age: 21 }
-
-delete human.age ;
-// object propertie sini silmek için delete kullanılır. ama kullanılmamalı.
-console.log(human);
-
-//propertie ler arasında döngü dönmek için for in kullanılır.
-txt = "";
-for(prop in human){
-  txt += human[prop] + " ";
-}
-console.log(txt);
-
-// normal for'u (indexli for'u ) for in şeklinde kullanmak:
-
-for(let i in human){
-  console.log(human[i]);
+function Man(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+  this.nationality = "English";
 }
 
-// propertie'lerin değeri olan value lar bir propery attribute'u dur
-// diğer attribute'lar : enumerable, configurable, and writable. bu attribute'lar,
-// propertie nin nasıl erişilceiğini belirler. okunabilir mi yazılabilir mi vs.
-// değiştirilebilen tek propertie attribute'u value'dur.
-// tüm property attribute'lar okunabilirdir.
+//prototype inheritance zincirinin en üstünde Object.prototype yer alır
+// ve diğer objectler ör: Man objecti , Date objecti , Array objecti prototype larını burdan alır.
 
+// burada Man functionuna Man.height = diye propertie ekleyemeyiz. ancak Man functionundan oluşan objectlere
+// ayrı ayrı propertie ekliyebiliriz. 
+// eğer Man constructor funct'dan oluşan tüm objectlere aynı propertie'yi veya method'u eklemek istersek 
+// veya constructor'un kendisine eklemek istersek 
+// prototype kullanırız.
 
-// object'e sonradan method ekleme:
+console.log(Man.prototype); //{}
 
-human.sayWelcome = function(guest){
-  return "Welcome " + guest.name + " " + guest.surname + " my name is " + this.name ;
+Man.prototype.homeland = "Hatay";
+
+console.log(Man.prototype); //{ homeland: 'Hatay' }
+
+const Abdullah = new Man("Abdullah","Aktaş",21,"brown");
+
+console.log(`Abdullah is from ${Abdullah.homeland}`);
+
+// prototype ile constructor function'a method da ekleyebiliriz.
+
+Man.prototype.sayOtherMansName = function(otherMan){
+  return otherMan.firstName + " " + otherMan.lastName;
 }
 
-const human2 = {
-  name: "Ali",
-  surname: "Yılmaz",
-  age:30,
-  sayAge : function(){
-    return `my age is ${this.age}`;
-  }
-}
+const Eren = new Man("Eren","Hedbe",21,"green");
 
-console.log(human.sayWelcome);
-console.log(human2.sayAge);
-console.log(human.sayWelcome(human2));
-
-console.log(human); 
-console.log(human2);
+console.log(Abdullah.sayOtherMansName(Eren));
