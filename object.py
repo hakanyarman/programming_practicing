3cef6e33c7865693b9382fb5a2b163630efd16bc
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-04-07 04:14:41 +0300

    oop python

---
+++ object.py
@@ -0,0 +1,36 @@
+# class içinde constructor function tanımlamak için :
+# def __init__(self): kullanılır. self'den sonra parametre olarak oluşturmak istediğimiz attrübutelar yazılır.
+
+class Car:
+    def __init__(self,model,year,color):
+        self.model = model #buradaki self js deki this ile aynı; objecti temsil ediyor.
+        self.year = year
+        self.color = color
+
+    def drive(self):
+        print("This "+ self.model + " car is driving")
+
+    def stop(self):
+        print("This "+ self.model + " car is stopped")
+
+
+
+my_car = Car("Ford",2020,"Black")
+your_car = Car("Audi",2010,"White")
+
+print(my_car.color)
+my_car.drive() #This Ford car is driving
+
+def which_is_new(car1,car2):
+    result = ""
+    if(car1.year > car2.year):
+        result = car2.model +  " is older than " + car1.model
+    elif(car2.year > car1.year):
+        result = car1.model +  " is older than " + car2.model
+    else:
+        result = car1.model + "'s year equal to " + car2.model + "'s"
+
+    return result
+
+print(which_is_new(my_car,your_car)) #Audi is older than Ford
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,18 @@
-obj2 = {"bir" : 1, "iki": 2, "üç": 3} #{'bir': 1, 'iki': 2, 'üç': 3}
-#print(dir(obj2))
-#print(help(obj2))
-print (obj2.get("bir")) 
-print(obj2.keys()) #dict_keys(['bir', 'iki', 'üç']) => sadece key leri yazdırdı.
 
-for key in obj2.keys():
-    print(key,obj2[key])
+class Car:
+    def __inti__(self,model,year,color):
+        self.model = model #buradaki self js deki this ile aynı; objecti temsil ediyor.
+        self.year = year
+        self.color = color
 
+    def drive():
+        print("This "+ self.model + " car is driving")
+
+    def stop():
+        print("This "+ self.model + " car is stopped")
+
+
+
+my_car = Car("Ford",2020,"Black")
+
+print(my_car)
