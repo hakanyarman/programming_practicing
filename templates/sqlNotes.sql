f815f9156b8c5f324c3c8a0b580b0e9ddca09de5
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-04-18 19:40:19 +0300

    added some sql notes

--- templates/sqlNotes.sql
+++ templates/sqlNotes.sql
@@ -21,7 +21,34 @@ SQL KOMUTLARI
 10)Create Index : index oluşturur
 11)Drop Index : indexi siler. 
 
+----------------------------
+<> : not equal veya != de kullanılıyor olabilir.
+BETWEEN : aralık belirtir. ör: 
+SELECT * FROM Products
+WHERE Price BETWEEN 50 AND 60; --> price ı 50 ile 60 arasında olanları getirir.
+LİKE: 
+SELECT * FROM Customers
+WHERE City LIKE 'e%'; --> e ile başlayan city leri getir.
+IN : çoklu koşul için veya || gibi. ör:
+SELECT * FROM Customers
+WHERE City IN ('Paris','London'); --> city si Paris veya London olan ların tüm columnlarını getir.
+AND - OR - NOT Operators: 
+Where ile kullanılır .
+ör:
+SELECT * FROM Customers
+WHERE (Country='Germany' AND City='Berlin') OR  (Country='Germany' AND City='München') OR (NOT Country = 'Germany'); 
 
+----------------------------------
+SELECT * FROM Customers
+ORDER BY Country, CustomerName;
+
+Burada Önce Country nin alfabetik sırasına göre sıralar, sonra aynı Country ler içinde
+CustomerName in alfabetik sırasına göre sıralar.
+-------------------------------
+
+INSERT INTO yaparken id yi biz belirtmiyoruz, yeni row eklediğimizde id otomatik olarak artıyor.
+
+Insert ederken tüm columnlara değer atamak zorunda değiliz. Değer vermediğimiz Columnlar null olur.
 
 
  */
