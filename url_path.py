6097cabc061af5fc1712cec040fa3a38eac9ee54
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-05-02 20:09:48 +0300

    dom manipulation practise

--- url_path.py
+++ url_path.py
@@ -2,16 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+@app.route("/")
+@make_bold 
+@make_emphasis  
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
 
 # username'den sonra girilen herhangi bir değerin name değişkenine atanmasını sağlar.
-@app.route("/username/<name>/<int:age>") # kullanıcının farklı değerler girdiğinde aynı route a erişmesini sağlar.
-def greet(name , age):
-    return f"Hello,{name},you are {age} years old "
+@app.route("/username/<name>")
+ # kullanıcının farklı değerler girdiğinde aynı route a erişmesini sağlar.
+def greet(name):
+    return f"Hello,{name}"
 # burada f harfi string formatlama metodu olan f-string için kullanılır. böylece string içerisinde
 # bir değişkeni süslü parantez içine alıp gösterebiliriz
 # ör: 
@@ -22,4 +37,3 @@ def greet(name , age):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
